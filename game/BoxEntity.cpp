f30a569105ff150b2a4ed363c5a8f3eb4553f642
Tomohiko Mukai <contact@mukai-lab.org>
2018-11-21 15:59:23 +0900

    正四面体の描画（法線適当）

--- game/BoxEntity.cpp
+++ game/BoxEntity.cpp
@@ -8,62 +8,34 @@ struct Vertex
     glm::vec3 normal;
 };
 
-const int numVertices = 24;
+const int numVertices = 12; // 正四面体（法線は適当に外向き）
 static Vertex vertex[numVertices] =
 {
-    // 面1 
-    { glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec3(0, 0, 1.0f) },
-    { glm::vec3(0.5f, -0.5f, 0.5f), glm::vec3(0, 0, 1.0f) },
-    { glm::vec3(-0.5f,  0.5f, 0.5f), glm::vec3(0, 0, 1.0f) },
-    { glm::vec3(0.5f,  0.5f, 0.5f), glm::vec3(0, 0, 1.0f) },
+    // 面1
+    { glm::vec3( 0.5,  0.5,  0.5), glm::vec3(0, 0, 1.0f) },
+    { glm::vec3( 0.5, -0.5, -0.5), glm::vec3(0, 0, 1.0f) },
+    { glm::vec3(-0.5,  0.5, -0.5), glm::vec3(0, 0, 1.0f) },
     // 面2 
-    { glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0, 0, -1.0f) },
-    { glm::vec3(0.5f, -0.5f, -0.5f), glm::vec3(0, 0, -1.0f) },
-    { glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0, 0, -1.0f) },
-    { glm::vec3(0.5f,  0.5f, -0.5f), glm::vec3(0, 0, -1.0f) },
+    { glm::vec3( 0.5,  0.5,  0.5), glm::vec3(0, 0, -1.0f) },
+    { glm::vec3( 0.5, -0.5, -0.5), glm::vec3(0, 0, -1.0f) },
+    { glm::vec3(-0.5, -0.5,  0.5), glm::vec3(0, 0, -1.0f) },
     // 面3 
-    { glm::vec3(0.5f,  0.5f, -0.5f), glm::vec3(0, 1.0f, 0) },
-    { glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0, 1.0f, 0) },
-    { glm::vec3(0.5f,  0.5f,  0.5f), glm::vec3(0, 1.0f, 0) },
-    { glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(0, 1.0f, 0) },
+    { glm::vec3( 0.5,  0.5,  0.5), glm::vec3(0, 1.0f, 0) },
+    { glm::vec3(-0.5,  0.5, -0.5), glm::vec3(0, 1.0f, 0) },
+    { glm::vec3(-0.5, -0.5,  0.5), glm::vec3(0, 1.0f, 0) },
     // 面4 
-    { glm::vec3(0.5f, -0.5f, -0.5f), glm::vec3(0, -1.0f, 0) },
-    { glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0, -1.0f, 0) },
-    { glm::vec3(0.5f, -0.5f,  0.5f), glm::vec3(0, -1.0f, 0) },
-    { glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(0, -1.0f, 0) },
-    // 面5 
-    { glm::vec3(0.5f,   0.5f, -0.5f), glm::vec3(1.0f, 0, 0) },
-    { glm::vec3(0.5f,  -0.5f, -0.5f), glm::vec3(1.0f, 0, 0) },
-    { glm::vec3(0.5f,   0.5f,  0.5f), glm::vec3(1.0f, 0, 0) },
-    { glm::vec3(0.5f,  -0.5f,  0.5f), glm::vec3(1.0f, 0, 0) },
-    // 面6 
-    { glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(-1.0f, 0, 0) },
-    { glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-1.0f, 0, 0) },
-    { glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(-1.0f, 0, 0) },
-    { glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(-1.0f, 0, 0) }
+    { glm::vec3( 0.5, -0.5, -0.5), glm::vec3(0, -1.0f, 0) },
+    { glm::vec3(-0.5,  0.5, -0.5), glm::vec3(0, -1.0f, 0) },
+    { glm::vec3(-0.5, -0.5,  0.5), glm::vec3(0, -1.0f, 0) },
 };
 
-const int numTriangles = 12;
+const int numTriangles = 4; // 
 static uint32_t modelIndex[numTriangles * 3] =
 {
-    // 面1 
     0, 1, 2,
-    1, 3, 2,
-    // 面2 
-    4, 6, 5,
-    5, 6, 7,
-    // 面3 
-    8, 9, 10,
-    9, 11, 10,
-    // 面4 
-    12, 14, 13,
-    13, 14, 15,
-    // 面5 
-    16, 18, 17,
-    17, 18, 19,
-    // 面6 
-    20, 21, 22,
-    21, 23, 22
+    3, 4, 5,
+    6, 7, 8,
+    9, 10, 11
 };
 
 BoxEntity::BoxEntity()
@@ -118,7 +90,7 @@ bool BoxEntity::Init()
 void BoxEntity::Update(const GameTime& time)
 {
     // 回転アニメーションの設定
-    SetLocalTransform(glm::rotate(LocalTransform(), 1.0e-2f * glm::pi<float>(), glm::vec3(0, 1.0f, 0)));
+    SetLocalTransform(glm::rotate(LocalTransform(), 1.0e-2f * glm::pi<float>(), glm::vec3(0, 1.0f, 0.0f)));
 
     Entity::Update(time);
 }
