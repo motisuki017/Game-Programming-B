7212241b5f7cdd25f57bf3921cc1421195061d91
Tomohiko Mukai <contact@mukai-lab.org>
2018-11-27 10:31:42 +0900

    PixelShaderデモのコード整理

--- fs/PlaneEntity.cpp
+++ fs/PlaneEntity.cpp
@@ -4,18 +4,6 @@
 #include "ObjFile.h"
 #include <iostream>
 
-static Vertex vertex[4] =
-{
-    { glm::vec3(-1.0, -1.0, 0), glm::vec3(0, 0, 1.0f) },
-    { glm::vec3( 1.0, -1.0, 0), glm::vec3(0, 0, 1.0f) },
-    { glm::vec3(-1.0,  1.0, 0), glm::vec3(0, 0, 1.0f) },
-    { glm::vec3( 1.0,  1.0, 0), glm::vec3(0, 0, 1.0f) }
-};
-static uint32_t modelIndex[6] = {
-    0, 1, 2,
-    1, 3, 2
-};
-
 PlaneEntity::PlaneEntity()
 {
 }
@@ -26,6 +14,21 @@ PlaneEntity::~PlaneEntity()
 
 bool PlaneEntity::Init()
 {
+    // スクリーンサイズに一致する、視線に垂直な平面
+    const int numVertices = 4;
+    Vertex vertex[numVertices] =
+    {
+        { glm::vec3(-1.0, -1.0, 0), glm::vec3(0, 0, 1.0f) },
+        { glm::vec3( 1.0, -1.0, 0), glm::vec3(0, 0, 1.0f) },
+        { glm::vec3(-1.0,  1.0, 0), glm::vec3(0, 0, 1.0f) },
+        { glm::vec3( 1.0,  1.0, 0), glm::vec3(0, 0, 1.0f) }
+    };
+    const int numTriangles = 2;
+    uint32_t modelIndex[numTriangles * 3] = {
+        0, 1, 2,
+        1, 3, 2
+    };
+
     // プログラムオブジェクト作成
     program = ShaderUtil::LoadProgram("effect.vert", "effect.frag");
     glUseProgram(program);
@@ -35,11 +38,11 @@ bool PlaneEntity::Init()
     // 頂点バッファ
     glGenBuffers(1, &vertexBufferObj);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObj);
-    glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), vertex, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(Vertex), vertex, GL_STATIC_DRAW);
     // インデックスバッファ
     glGenBuffers(1, &indexBufferObj);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObj);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), modelIndex, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, numTriangles * 3 * sizeof(uint32_t), modelIndex, GL_STATIC_DRAW);
     // 頂点バッファオブジェクトの頂点シェーダ入力への紐付け
     glBindAttribLocation(program, 0, "position");
     glEnableVertexAttribArray(0);
@@ -47,28 +50,35 @@ bool PlaneEntity::Init()
     glBindAttribLocation(program, 1, "normal");
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(12));
-    // uniform変数
+
+    // カメラ行列
     glm::mat4 mView = glm::lookAt(glm::vec3(0, 0.0f, 1.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1.0f, 0));
-    glm::mat4 mProjection = glm::orthoRH(-1.0f, 1.0f, -1.0f, 1.0f, 1.0e-3f, 1.0e3f);
-    uidView = glGetUniformLocation(program, "mView");     // カメラ行列
+    uidView = glGetUniformLocation(program, "mView");
     glUniformMatrix4fv(uidView, 1, GL_FALSE, glm::value_ptr(mView));
-    uidProjection   = glGetUniformLocation(program, "mProjection");     // 射影変換行列
+    // 射影変換行列
+    glm::mat4 mProjection = glm::orthoRH(-1.0f, 1.0f, -1.0f, 1.0f, 1.0e-3f, 1.0e3f);
+    uidProjection   = glGetUniformLocation(program, "mProjection");
     glUniformMatrix4fv(uidProjection, 1, GL_FALSE, glm::value_ptr(mProjection));
-    uidTime = glGetUniformLocation(program, "time");
-    uidMousePos = glGetUniformLocation(program, "mousePos");
+    // ウィンドウサイズ
     uidWindowSize = glGetUniformLocation(program, "windowSize");
+    glm::vec2 windowSize = owner->WindowSize();
+    glUniform2f(uidWindowSize, windowSize.x, windowSize.y);
+    // 経過時間
+    uidTime = glGetUniformLocation(program, "time");
+    // マウス位置
+    uidMousePos = glGetUniformLocation(program, "mousePos");
 
     return Entity::Init();
 }
 
 void PlaneEntity::Update(const GameTime& time)
 {
+    // 現在の経過時間をセット
     glUniform1f(uidTime, time.TotalTime());
+    // 現在のマウス位置をセット
     glm::vec2 mousePos = owner->MousePos();
     glUniform2f(uidMousePos, mousePos.x, mousePos.y);
-    glm::vec2 windowSize = owner->WindowSize();
-    glUniform2f(uidWindowSize, windowSize.x, windowSize.y);
-    std::cout << mousePos.x << ", " << mousePos.y << std::endl;
+
     Entity::Update(time);
 }
 
